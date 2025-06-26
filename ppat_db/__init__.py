f8a02cb2065497c0f3fb928308f8f305d4e2bc48
hunseop <50357363+hunseop@users.noreply.github.com>
2025-06-26 15:12:19 +0900

    Update __init__.py: PolicyDB 없는 메서드 제거

--- ppat_db/__init__.py
+++ ppat_db/__init__.py
@@ -3,9 +3,8 @@
 정책 데이터베이스 관리 기능을 제공합니다.
 """
 
-from .policy_db import PolicyDB, save_policy_to_db
+from .policy_db import save_policy_to_db
 
 __all__ = [
-    'PolicyDB',
     'save_policy_to_db'
 ]
